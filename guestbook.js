3def069626d14d37e330107f7697b7e8d1e2740f
iwpon <111081526+iwpon@users.noreply.github.com>
2022-08-16 16:11:06 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f34e585f.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
